5315133cfc9eeaaff36848d4e5addfb7c0d97fec
David Wilson <dw@botanicus.net>
2013-03-23 10:59:29 +0000

    Add snappy to batch support too.

--- examples/batch.py
+++ examples/batch.py
@@ -9,6 +9,9 @@ lines = file('/etc/services').readlines()
 header = random.sample(lines, 150)
 footer = random.sample(lines, 150)
 
+recs = [header + random.sample(lines, 30) + footer
+        for _ in xrange(400)]
+
 def dotest():
     t0 = time.time()
     cnt = 0
@@ -18,23 +21,32 @@ def dotest():
             cnt += 1
     return cnt / (time.time() - t0)
 
+try:
+    import snappy
+    SNAPPY_PACKER = centidb.Encoder('snappy',
+        snappy.uncompress,
+        snappy.compress)
+except ImportError:
+    SNAPPY_PACKER = None
 
-for bsize in 1, 2, 4, 5, 8, 16, 32, 64:
-    le = centidb.support.ListEngine()
-    st = centidb.Store(le)
-    co = centidb.Collection(st, 'people')
 
-    keys = []
-    for x in range(400):
-        data = header + random.sample(lines, 30) + footer
-        keys.append(co.put(data).key)
+for packer in centidb.ZLIB_PACKER, SNAPPY_PACKER:
+    if not packer:
+        continue
 
-    before = le.size
+    print
+    for bsize in 1, 2, 4, 5, 8, 16, 32, 64:
+        le = centidb.support.ListEngine()
+        st = centidb.Store(le)
+        co = centidb.Collection(st, 'people')
 
-    if bsize == 1:
-        print 'Before size %7.2fkb count %4d %26s (%4.2f gets/sec)' %\
-            (before / 1024., len(le.pairs), '', dotest())
-    co.batch(max_recs=bsize, packer=centidb.ZLIB_PACKER)
+        keys = [co.put(rec).key for rec in recs]
+        before = le.size
 
-    print ' After size %7.2fkb count %4d ratio %5.2f (batch size %2d, %4.2f gets/sec)' %\
-        (le.size / 1024., len(le.pairs), float(before) / le.size, bsize, dotest())
+        if bsize == 1:
+            print 'Before size %7.2fkb count %4d %28s (%4.2f gets/sec)' %\
+                (before / 1024., len(le.pairs), '', dotest())
+        co.batch(max_recs=bsize, packer=packer)
+
+        print ' After size %7.2fkb count %4d ratio %5.2f (%7s size %2d, %4.2f gets/sec)' %\
+            (le.size / 1024., len(le.pairs), float(before) / le.size, packer.name, bsize, dotest())
