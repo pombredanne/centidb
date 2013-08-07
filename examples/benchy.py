5cebe2a0e2d24d9f0f80b4d83eed60d1a133c368
David Wilson <dw@botanicus.net>
2013-08-07 12:17:25 +0100

    Add mongo to benchy

--- examples/benchy.py
+++ examples/benchy.py
@@ -10,6 +10,7 @@ import time
 
 import centidb
 import centidb.support
+import pymongo
 
 writer = csv.writer(sys.stdout, quoting=csv.QUOTE_ALL)
 out = lambda *args: writer.writerow(args)
@@ -18,16 +19,76 @@ BASE_PATH = '/ram/benchy/'
 if not os.path.exists(BASE_PATH):
     os.mkdir(BASE_PATH, 0744)
 
-ENGINES = [
-    ('lmdb', BASE_PATH + 'test.lmdb',
-     lambda: centidb.open('LmdbEngine', path=BASE_PATH + 'test.lmdb',
-                          map_size=1048576*1024)),
-    ('skiplist', None,
-     lambda: centidb.open('SkiplistEngine', maxsize=int(1e9))),
-    ('plyvel', BASE_PATH + 'test.ldb',
-     lambda: centidb.open('PlyvelEngine', name=BASE_PATH + 'test.ldb',
-                          create_if_missing=True)),
-]
+
+class CentiEngine(object):
+    ENCODER = centidb.support.make_msgpack_encoder()
+    KEY_FUNC = operator.itemgetter('name', 'location')
+
+    def create(self):
+        if self.PATH and os.path.exists(self.PATH):
+            shutil.rmtree(self.PATH)
+        self.make_engine()
+
+    def make_coll(self, use_indices):
+        self.coll = self.store.collection('stuff',
+            encoder=self.ENCODER, key_func=self.KEY_FUNC)
+        if use_indices:
+            self.coll.add_index('rev_name', lambda p: p['name'][::-1])
+            self.coll.add_index('rev_locn', lambda p: p['location'][::-1])
+
+    def close(self):
+        self.store.engine.close()
+        shutil.rmtree(self.PATH)
+
+    def insert(self, words, upper, stub, blind):
+        coll = self.coll
+        txn = self.store.engine.begin(write=True)
+        for i in xrange(len(words)):
+            doc = {'stub': stub, 'name': words[i], 'location': upper[i]}
+            coll.put(doc, blind=blind, txn=txn)
+        txn.commit()
+
+
+class LmdbEngine(CentiEngine):
+    PATH = BASE_PATH + 'test.lmdb'
+    def make_engine(self):
+        self.store = centidb.open('LmdbEngine',
+            path=self.PATH, map_size=1048576*1024)
+
+
+class SkiplistEngine(CentiEngine):
+    PATH = None
+    def make_engine(self):
+         self.store = centidb.open('SkiplistEngine', maxsize=int(1e9))
+
+
+class PlyvelEngine(CentiEngine):
+    PATH = BASE_PATH + 'test.ldb'
+
+    def make_engine(self):
+        self.store = centidb.open('PlyvelEngine', name=self.PATH,
+                                  create_if_missing=True)
+
+
+class MongoEngine(object):
+    def create(self):
+        pass
+
+    def make_coll(self, use_indices):
+        self.store = pymongo.MongoClient()
+        self.store.drop_database('benchy')
+        os.system('sudo rm -rf /ram/mongodb/benchy*')
+        self.store.benchy.create_collection('stuff')
+        self.coll = self.store.benchy.stuff
+        if use_indices:
+            self.coll.ensure_index('name', 1)
+            self.coll.ensure_index('location', 1)
+
+    def insert(self, words, upper, stub, blind):
+        coll = self.coll
+        for i in xrange(len(words)):
+            doc = {'stub': stub, 'name': words[i], 'location': upper[i]}
+            coll.insert(doc)
 
 
 def x():
@@ -38,34 +99,23 @@ def x():
     upper = map(str.upper, words)
     stub = len('%x' % (random.getrandbits(150*4)))
 
-    encoder = centidb.support.make_msgpack_encoder()
-    key_func = operator.itemgetter('name', 'location')
-
     out('Engine', 'Blind', 'UseIndices', 'Keys', 'Time', 'Ops/s')
-    for engine_name, engine_path, engine_factory in ENGINES:
+    for engine in LmdbEngine, SkiplistEngine, PlyvelEngine, MongoEngine:
         for blind in True, False:
+            print
             for use_indices in True, False:
-                if engine_path and os.path.exists(engine_path):
-                    shutil.rmtree(engine_path)
-                store = engine_factory()
-
-                coll = store.collection('stuff',
-                    encoder=encoder, key_func=key_func)
-                if use_indices:
-                    coll.add_index('rev_name', lambda p: p['name'][::-1])
-                    coll.add_index('rev_locn', lambda p: p['location'][::-1])
+                eng = engine()
+                eng.create()
+                eng.make_coll(use_indices)
 
                 t0 = time.time()
-                txn = store.engine.begin(write=True)
-                for i in xrange(len(words)):
-                    doc = {'stub': stub, 'name': words[i], 'location': upper[i]}
-                    coll.put(doc, blind=blind, txn=txn)
-                txn.commit()
-                store.engine.close()
+                eng.insert(words, upper, stub, blind)
+                eng.close()
                 t = time.time() - t0
 
-                keycnt = len(words) * (1 + len(coll.indices))
+                keycnt = len(words) * (3 if use_indices else 1)
+                engine_name = engine.__name__
                 out(engine_name, blind, use_indices, keycnt, '%.2f' % t,
-                    '%.2f' % ((keycnt if blind else (keycnt + len(words))) / t)
-)
+                    '%.2f' % ((keycnt if blind else (keycnt + len(words))) / t))
+
 x()
