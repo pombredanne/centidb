99601271af1a8ffa3f912a137c42d8c08b99325e
David Wilson <dw@botanicus.net>
2013-08-15 00:23:24 +0000

    Fix module ref.

--- centidb/keycoder.py
+++ centidb/keycoder.py
@@ -414,6 +414,6 @@ def unpack(prefix, s):
 if os.path.basename(sys.argv[0]) not in ('sphinx-build', 'pydoc') and \
         os.getenv('CENTIDB_NO_SPEEDUPS') is None:
     try:
-        from _keycoder import *
+        from centidb._keycoder import *
     except ImportError:
         pass
