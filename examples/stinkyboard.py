0a46dc1b90e42df01b6a54bf626021e2b86a1a44
David Wilson <dw@botanicus.net>
2013-08-28 01:00:25 +0100

    Swap Flask/Jinja2 out for bjoern/wheezy.template.

--- examples/stinkyboard.py
+++ examples/stinkyboard.py
@@ -4,10 +4,20 @@ import time
 import centidb
 import flask
 
+import wheezy.template.engine
+import wheezy.template.ext.core
+import wheezy.template.loader
+
+templates = wheezy.template.engine.Engine(
+    loader=wheezy.template.loader.FileLoader(['templates']),
+    extensions=[
+        wheezy.template.ext.core.CoreExtension()
+])
+
 store = centidb.open('LmdbEngine', path='store.lmdb', map_size=512e6)
 store.add_collection('posts')
 
-app = flask.Flask(__name__, template_folder='templates')
+app = flask.Flask(__name__)
 
 
 def getint(name, default=None):
@@ -19,12 +29,14 @@ def getint(name, default=None):
 
 @app.route('/')
 def index():
+    t0 = time.time()
     lo = getint('lo')
     hi = getint('hi')
     if lo:
         posts = list(store['posts'].items(max=5, lo=lo))[::-1]
     else:
         posts = list(store['posts'].items(hi=hi, reverse=True, max=5))
+    t1 = time.time()
 
     newer = older = None
     if posts:
@@ -34,8 +46,12 @@ def index():
         if hi:
             newer = '?lo=' + str(hi)
 
-    return flask.render_template('index.html', posts=posts,
-                                 older=older, newer=newer)
+    return templates.get_template('index.html').render({
+        'posts': posts,
+        'older': older,
+        'newer': newer,
+        'msec': int((t1 - t0) * 1000)
+    })
 
 
 @app.route('/newpost', methods=['POST'])
@@ -45,5 +61,10 @@ def newpost():
     rec = store['posts'].put(post)
     return flask.redirect('.')
 
+import sys
 
-app.run(host='0.0.0.0', port=8000, debug=True)
+if 'debug' in sys.argv:
+    app.run(host='0.0.0.0', port=8000, debug=True)
+else:
+    import bjoern
+    bjoern.run(app, '0.0.0.0', 8000)
