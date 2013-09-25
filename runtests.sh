ffc89aeef083c92d1179ed6f4b6699d2fd417a07
David Wilson <dw@botanicus.net>
2013-09-25 19:35:27 +0100

    Allow passing params to unittest.py.

--- runtests.sh
+++ runtests.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-PYTHONPATH=tests python -munittest core_test engines_test iterator_test keylib_test meta_test
+PYTHONPATH=tests python -munittest "$@" core_test engines_test iterator_test keylib_test meta_test
